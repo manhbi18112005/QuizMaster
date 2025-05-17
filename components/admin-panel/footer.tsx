c92b84147da9370229465bf69b918db20758fd9e
Le Ngo Duc Manh <myt@nnsvn.me>
2025-05-17 23:26:47 +0700

    1.0.1
    Changed the footer text

--- components/admin-panel/footer.tsx
+++ components/admin-panel/footer.tsx
@@ -5,23 +5,23 @@ export function Footer() {
     <div className="z-20 w-full bg-background/95 shadow backdrop-blur supports-[backdrop-filter]:bg-background/60">
       <div className="mx-4 md:mx-8 flex h-14 items-center">
         <p className="text-xs md:text-sm leading-loose text-muted-foreground text-left">
-          Built on top of{" "}
+          Made with ❤️ by{" "}
           <Link
-            href="https://ui.shadcn.com"
+            href="https://github.com/manhbi18112005/QuizMaster"
             target="_blank"
             rel="noopener noreferrer"
             className="font-medium underline underline-offset-4"
           >
-            shadcn/ui
+            No Name Studio
           </Link>
-          . The source code is available on{" "}
+          . Open source under the{" "}
           <Link
-            href="https://github.com/salimi-my/shadcn-ui-sidebar"
+            href="https://opensource.org/licenses/GPL-3.0"
             target="_blank"
             rel="noopener noreferrer"
             className="font-medium underline underline-offset-4"
           >
-            GitHub
+            GPL-3.0 License
           </Link>
           .
         </p>
