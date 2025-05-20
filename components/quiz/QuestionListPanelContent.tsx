13d8dad2611a14b1ce2ed33215198d8d92ee17e9
Le Ngo Duc Manh <myt@nnsvn.me>
2025-05-20 09:50:32 +0700

    temporaily removed simplebar

--- components/quiz/QuestionListPanelContent.tsx
+++ components/quiz/QuestionListPanelContent.tsx
@@ -5,10 +5,6 @@ import {
 } from '@dnd-kit/sortable';
 import { Question } from '@/types/quiz';
 import { QuestionCard } from './QuestionCard';
-
-import SimpleBar from 'simplebar-react';
-import 'simplebar-react/dist/simplebar.min.css';
-
 interface QuestionListPanelContentProps {
   questions: Question[];
   selectedQuestionId: string | null;
@@ -40,29 +36,27 @@ export const QuestionListPanelContent: FC<QuestionListPanelContentProps> = ({
       strategy={verticalListSortingStrategy}
     >
       <div className="flex h-100 w-full flex-col gap-2.5 p-3">
-        <SimpleBar>
-          {questions.length === 0 && <p className="text-muted-foreground">No questions yet. Click create to add one.</p>}
-          {questions.map(q => (
-            <div
-              key={q.id}
-              ref={el => {
-                if (el) {
-                  itemRefs.current.set(q.id, el);
-                } else {
-                  itemRefs.current.delete(q.id);
-                }
-              }}
-            >
-              <QuestionCard
-                // key={q.id} // Key is now on the parent div
-                id={q.id}
-                question={q}
-                onClick={() => onCardClick(q.id)}
-                isSelected={q.id === selectedQuestionId}
-              />
-            </div>
-          ))}
-        </SimpleBar>
+        {questions.length === 0 && <p className="text-muted-foreground">No questions yet. Click create to add one.</p>}
+        {questions.map(q => (
+          <div
+            key={q.id}
+            ref={el => {
+              if (el) {
+                itemRefs.current.set(q.id, el);
+              } else {
+                itemRefs.current.delete(q.id);
+              }
+            }}
+          >
+            <QuestionCard
+              // key={q.id} // Key is now on the parent div
+              id={q.id}
+              question={q}
+              onClick={() => onCardClick(q.id)}
+              isSelected={q.id === selectedQuestionId}
+            />
+          </div>
+        ))}
       </div>
     </SortableContext>
   );
