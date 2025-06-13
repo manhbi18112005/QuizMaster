fe58dc3b6039a04b8ee1a5ed1b60341477d841eb
Le Ngo Duc Manh <myt@nnsvn.me>
2025-06-13 11:52:58 +0700

    Fix invalid fallback image

--- lib/utils/avatar.ts
+++ lib/utils/avatar.ts
@@ -2,9 +2,9 @@ import { User } from "next-auth";
 import { GitHubUser } from "@/types/github-user";
 
 export function getAvatarUrl(user: (GitHubUser & User) | null | undefined): string {
-    if (!user) return '/default-avatar.png';
+    if (!user) return OG_AVATAR_URL;
 
-    return user.avatar_url || user.image || '/default-avatar.png';
+    return user.avatar_url || user.image || OG_AVATAR_URL;
 }
 
 export function getAvatarFallback(user: (GitHubUser & User) | null | undefined): string {
